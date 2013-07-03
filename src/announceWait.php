8be8a3935e47e91d1f4aae503242f00929be1a86
Philip Graham <philip.robert.graham@gmail.com>
2013-07-03 21:39:36 +0000

    Added announceWait function

---
+++ src/announceWait.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Simple function that will output a countdown.  This is useful for giving the
+ * user a chance to cancel a pending operation that can have undesired, hard to
+ * reverse changes.
+ *
+ * @param waitTime {integer}
+ *            Number of seconds to count down before returning
+ * @param message {string} default: 'Commencing'
+ *            The message will echoed with the suffix 'in (Ctrl-C to cancel)'
+ *            appended.
+ * 
+ * @author Philip Graham <philip@zeptech.ca>
+ */
+function announceWait($waitTime, $message = 'Commencing') {
+  echo "\n";
+
+  $message .= ' in';
+  for ($i = 0; $i < strlen($message); $i++) {
+    echo substr($message, $i, 1);
+  }
+  echo "    (Ctrl-C to cancel)\n";
+  sleep(1);
+
+  for ($i = $waitTime; $i > 0; $i--) {
+      echo $i.'... ';
+      if ($i == 1) {
+        echo "\n";
+      }
+      sleep(1);
+  }
+  echo "\n";
+}
