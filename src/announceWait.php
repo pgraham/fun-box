aeb990e920fe1c2857077c52dad04821e4c41dc9
Philip Graham <philip.robert.graham@gmail.com>
2013-07-04 15:32:41 +0000

    Added BSD license to source

--- src/announceWait.php
+++ src/announceWait.php
@@ -1,5 +1,19 @@
 <?php
 /**
+ * =============================================================================
+ * Copyright (c) 2013, Philip Graham
+ * All rights reserved.
+ *
+ * This file is part of Zeptech FunBox and is licensed by the Copyright holder
+ * under the 3-clause BSD License.  The full text of the license can be found in 
+ * the LICENSE.txt file included in the root directory of this distribution or
+ * at the link below.
+ * =============================================================================
+ *
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
+
+/**
  * Simple function that will output a countdown.  This is useful for giving the
  * user a chance to cancel a pending operation that can have undesired, hard to
  * reverse changes.
--- src/ensureFn.php
+++ src/ensureFn.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * =============================================================================
+ * Copyright (c) 2013, Philip Graham
+ * All rights reserved.
+ *
+ * This file is part of Zeptech FunBox and is licensed by the Copyright holder
+ * under the 3-clause BSD License.  The full text of the license can be found in
+ * the LICENSE.txt file included in the root directory of this distribution or
+ * at the link below.
+ * =============================================================================
+ *
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
 namespace zpt\fn {
 
 /**
