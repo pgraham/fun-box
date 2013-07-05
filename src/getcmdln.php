654ac44e68a5468bbbeda1b09c719c3692d8b389
Philip Graham <philip.robert.graham@gmail.com>
2013-07-05 14:41:27 +0000

    Added getcmdln function which parses an argv array for longoptions and arguments

---
+++ src/getcmdln.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * =============================================================================
+ * Copyright (c) 2013, Philip Graham
+ * All rights reserved.
+ *
+ * This file is part of Zeptech FunBox and is licensed by the Copyright holder
+ * under the 3-clause BSD License.  The full text of the license can be found in
+ * the LICENSE.txt file included in the root directory of this distribution or
+ * at the link below.
+ * =============================================================================
+ *
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
+namespace zpt\fn {
+
+use \StdClass;
+
+/**
+ * Static class wrapper for the getcmdln function.
+ *
+ * @author Philip Graham <philip@zeptech.ca>
+ */
+class GetCmdln {
+
+	const LONG_OPT_RE = '/--(\w+)(?:=(\S+))?/';
+
+	/**
+	* This function retrieves command line options and arguments.	Return format is 
+	* a stdClass instance with three properties:
+	*
+	*  -  `opt` : array in the format of getopt() but arguments don't need to be 
+	*             specified
+	*  -  `argv`: $argv - `opt`
+	*  -  `argc`: Number of arguments in `argv`
+	*
+	* @param string $options `options` argument to getopt()
+	* @param array $longopts `longopts` argument to getopt()
+	*/
+	public static function parse($argv, $options, $longopts = array()) {
+		array_shift($argv);
+
+		$opts = array();
+		$args = array();
+		foreach ($argv as $arg) {
+			if (preg_match(self::LONG_OPT_RE, $arg, $matches)) {
+				$opts[$matches[1]] = $matches[2];
+			} else {
+				$args[] = $arg;
+			}
+		}
+
+		$cmdln = new StdClass();
+		$cmdln->opt = $opts;
+		$cmdln->argv = $args;
+		$cmdln->argc = count($args);
+		return $cmdln;
+	}
+}
+
+} // End zpt\fn namespace
+
+namespace {
+
+	/**
+	 * Global namespace alias for GetCmdln::parse. Only supports long options.
+	 */
+	function getcmdln($argv, $longopts = array()) {
+		return zpt\fn\GetCmdln::parse($argv, '', $longopts);
+	}
+}
---
+++ test/getcmdln_test.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * =============================================================================
+ * Copyright (c) 2013, Philip Graham
+ * All rights reserved.
+ *
+ * This file is part of Zeptech FunBox and is licensed by the Copyright holder
+ * under the 3-clause BSD License.  The full text of the license can be found in
+ * the LICENSE.txt file included in the root directory of this distribution or
+ * at the link below.
+ * =============================================================================
+ *
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit_Framework_TestCase as TestCase;
+ensureFn('getcmdln');
+
+/**
+ * This class tests the getcmdln function.
+ *
+ * @author Philip Graham <philip@zeptech.ca>
+ */
+class GetcmdlnTest extends TestCase {
+
+	public function testLongOptions() {
+		$argv = array(
+			'script.php',
+			'--longoption=myvalue',
+			'--anotheroption=myvalue',
+		);
+		$cmdln = getcmdln($argv, array('longoption', 'anotheroption'));
+
+		$this->assertInstanceOf('StdClass', $cmdln);
+		$this->assertObjectHasAttribute('opt', $cmdln);
+		$this->assertInternalType('array', $cmdln->opt);
+
+		$this->assertArrayHasKey('longoption', $cmdln->opt);
+		$this->assertEquals('myvalue', $cmdln->opt['longoption']);
+		$this->assertArrayHasKey('anotheroption', $cmdln->opt);
+		$this->assertEquals('myvalue', $cmdln->opt['anotheroption']);
+	}
+
+	public function testArgs() {
+		$argv = array(
+			'script.php',
+			'arg1',
+			'arg2'
+		);
+		$cmdln = getcmdln($argv);
+
+		$this->assertInstanceOf('StdClass', $cmdln);
+		$this->assertObjectHasAttribute('argv', $cmdln);
+		$this->assertInternalType('array', $cmdln->argv);
+
+		$this->assertObjectHasAttribute('argc', $cmdln);
+		$this->assertInternalType('int', $cmdln->argc);
+		$this->assertEquals(2, $cmdln->argc);
+
+		$this->assertCount(2, $cmdln->argv);
+		$this->assertEquals(array('arg1', 'arg2'), $cmdln->argv);
+	}
+
+	public function testLongOptionsWithArgsExpectedOrder() {
+		$argv = array(
+			'script.php',
+			'--longoption=myvalue',
+			'--anotheroption=myvalue',
+			'arg1',
+			'arg2'
+		);
+		$cmdln = getcmdln($argv, array('longoption', 'anotheroption'));
+
+		$this->assertInstanceOf('StdClass', $cmdln);
+		$this->assertObjectHasAttribute('opt', $cmdln);
+		$this->assertInternalType('array', $cmdln->opt);
+
+		$this->assertArrayHasKey('longoption', $cmdln->opt);
+		$this->assertEquals('myvalue', $cmdln->opt['longoption']);
+		$this->assertArrayHasKey('anotheroption', $cmdln->opt);
+		$this->assertEquals('myvalue', $cmdln->opt['anotheroption']);
+
+		$this->assertObjectHasAttribute('argv', $cmdln);
+		$this->assertInternalType('array', $cmdln->argv);
+
+		$this->assertObjectHasAttribute('argc', $cmdln);
+		$this->assertInternalType('int', $cmdln->argc);
+		$this->assertEquals(2, $cmdln->argc);
+
+		$this->assertCount(2, $cmdln->argv);
+		$this->assertEquals(array('arg1', 'arg2'), $cmdln->argv);
+	}
+}
